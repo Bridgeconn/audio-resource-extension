060f82403085ce211b54ca7b05eb74284bfce4ee
sijumoncy <72241997+sijumoncy@users.noreply.github.com>
2024-05-14 11:37:10 +0530

    added supported files , types etc

--- src/types/ExtToUIMsg.ts
+++ src/types/ExtToUIMsg.ts
@@ -1,1 +1,8 @@
 // Types for messages shared from Extension to UI
+import { IVerseData, IChapterdata } from '.';
+
+export enum ExttoUIWebMsgTypes {
+  ChapterData = 'Reference Chapter Data of IChapterdata',
+}
+
+type ExtToEditorMsgDataType = ExttoUIWebMsgTypes.ChapterData;
---
+++ src/types/storageKeys.ts
@@ -0,0 +1,7 @@
+export enum storageKeys {
+  currentBC = 'Current selected book and chapter',
+  versification = 'Versification data of current project',
+  workspaceDirectory = 'Project root directory',
+  loadedUSFMContent = 'store loaded and parsed content to reuse',
+  metadataJSON = 'Storing the metadata of the project',
+}
